d22325d8d4fb056fb0f61acefabd08bed4844a0c
Alexander Kuzkin <xbevice@gmail.com>
2018-10-16 15:46:50 +0300

    Initial

--- drivers/hal_stm32f0xx.cmake
+++ drivers/hal_stm32f0xx.cmake
@@ -49,6 +49,7 @@ set(STM32F0XX_HAL_HEADERS
         )
 
 set(STM32F0XX_HAL_SOURCES
+        STM32F0xx_HAL_Driver/Src/stm32f0xx_hal.c
         STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_pwr.c
         STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_smartcard_ex.c
         STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_comp.c
--- src/main.c
+++ src/main.c
@@ -8,20 +8,20 @@ int main(void) {
 
 
     HAL_Init();
-    clock_set_pre(CLOCK_SOURCE_PLL, CLOCK_AHB_PRE_1, CLOCK_APB_PRE_1, CLOCK_APB_PRE_1);
+    clock_set_pre(CLOCK_SOURCE_PLL, CLOCK_AHB_PRE_1, CLOCK_APB_PRE_1);
     clock_set_systick(CLOCK_DEFAULT_SYSTICK_FQ);
-    clock_start(CLOCK_SOURCE_PLL, 8000000, CLOCK_PLL_MUL_9);
+    clock_start(CLOCK_SOURCE_PLL, 8000000, CLOCK_PLL_MUL_6);
 
 
-    __HAL_RCC_GPIOC_CLK_ENABLE();
+    __HAL_RCC_GPIOA_CLK_ENABLE();
     GPIO_InitTypeDef g;
     g.Mode = GPIO_MODE_OUTPUT_PP;
     g.Speed = GPIO_SPEED_FREQ_LOW;
-    g.Pin = GPIO_PIN_13;
-    HAL_GPIO_Init(GPIOC, &g);
+    g.Pin = GPIO_PIN_4;
+    HAL_GPIO_Init(GPIOA, &g);
 
     while (1) {
-        HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
+        HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_4);
         HAL_Delay(1000);
     }
 
